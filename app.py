861b92023f11c5d7a0d7be60d7aacc698b0cb241
quang <quangdangp@student.unimelb.edu.au>
2026-01-09 16:05:46 +0700

    Adjust

--- app.py
+++ app.py
@@ -251,7 +251,7 @@ if uploaded: #Uploaded excel file update
                     StaggingMonth = st.number_input("Start Month Stagging", min_value=1, max_value=12, step=1, value=1)
 
 
-        ShopVisitPurpose = st.selectbox("Shop Visit Scope", ["Engine Performance Restoration 1", "Engine Performance Restoration 2", "LLPs"])
+        ShopVisitPurpose = st.selectbox("Shop Visit Scope", ["FirstVist", "Engine Performance Restoration 2", "LLPs"])
 
     with col10:
         CleanStagging = st.selectbox("Cleaning Option", ["Manual", "Automatic"])
